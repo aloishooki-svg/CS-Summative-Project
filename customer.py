56a765263ca51e23985b6a5abe795e47aed2dfba
sbarnabas-prog <s.barnabas@alustudent.com>
2026-09-05 12:09:04 +0400

    first commit

---
+++ customer.py
@@ -0,0 +1,10 @@
+# The Customer class represents a customer in the vehicle management system. Each customer object has its own personal details that will store information about the customer. 
+class Customer:
+    def __init__(self, customer_id, customer_name, phone_number,email, driver_license):
+        self.customer_id = customer_id
+        self.customer_name = customer_name
+        self.phone_number = phone_number
+        self.email = email
+        self.driver_license = driver_license
+# Phone number and email are kept separate so that each can be validated and displayed independently. 
+
