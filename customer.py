60e65731f37610833dace34c1d9e33f98de01c3e
sbarnabas-prog <s.barnabas@alustudent.com>
2026-09-07 14:55:16 +0400

    Created the customer class and displayed all the information concerning the customer

--- customer.py
+++ customer.py
@@ -1,11 +1,18 @@
 # The Customer class represents a customer in the vehicle management system. Each customer object has its own personal details that will store information about the customer. 
+# The customer class also does not contain the list of rented vehicles, as this information can be found in the rental class.
 class Customer:
     def __init__(self, customer_id, customer_name, phone_number,email, driver_license):
         self.customer_id = customer_id
         self.customer_name = customer_name
-        self.phone_number = phone_number
+        self.phone_number = phone_number      # Phone number and email are kept separate so that each can be validated and displayed independently. 
         self.email = email
         self.driver_license = driver_license
-# Phone number and email are kept separate so that each can be validated and displayed independently. 
-# The customer class also does not contain the list of rented vehicles, as this information can be found in the rental class.
+
+    # Display the customer information in a format that will be easy to read by the user.
+    def display_customer_info(self):
+        print(f"Customer ID: {self.customer_id}")
+        print(f"Customer Name: {self.customer_name}")
+        print(f"Phone Number: {self.phone_number}")
+        print(f"Email Address: {self.email}")
+        print(f"Driver License: {self.driver_license}")
 
