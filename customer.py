da16407556a341757294a7cdd2bcfbdee304a5d7
sbarnabas-prog <s.barnabas@alustudent.com>
2026-09-09 21:02:44 +0400

    Added input validation in the customer class

--- customer.py
+++ customer.py
@@ -2,6 +2,17 @@
 # The customer class also does not contain the list of rented vehicles, as this information can be found in the rental class.
 class Customer:
     def __init__(self, customer_id, customer_name, phone_number,email, driver_license):
+        #Adding validation to customer_id, customer_name, driver_license, and phone_number to ensure that an incomplete customer records are not
+        #stored in the system.
+        if not customer_id:
+            raise ValueError("Customer ID cannot be empty.")
+        if not customer_name:
+            raise ValueError("Customer name cannot be empty.")
+        if not driver_license:
+            raise ValueError("Driver license cannot be empty.")
+        if not phone_number:
+            raise ValueError("Phone number cannot be empty.")
+
         self.customer_id = customer_id
         self.customer_name = customer_name
         self.phone_number = phone_number      # Phone number and email are kept separate so that each can be validated and displayed independently. 
